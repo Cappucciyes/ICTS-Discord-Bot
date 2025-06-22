07b6497baa6e88058e8dbab7967a4934cf7c503c
Cappucciyes <ltkproper@gmail.com>
2025-06-23 03:42:03 +0900

    test for firstJoin() function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 1. 주요 클래스 가져오기
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { getJson, getRecentSolved } = require('./baekjoon.js');
+const { getJson, getRecentSolved, firstJoin } = require('./baekjoon.js');
 require('dotenv').config();
 const token = process.env.DISCORD_TOKEN;
 const serverID = process.env.SERVER_ID;
@@ -36,8 +36,20 @@ client.on('messageCreate', (message) => {
         clearInterval(inter)
     }
 
-    if (message.content == "/모두푼") {
-        checkAllUser();
+    // if (message.content == "/모두푼") {
+    //     checkAllUser();
+    // }
+
+    if (message.content.startsWith("/등록")) {
+        const parts = message.content.split(" ");
+        if (parts.length < 2) {
+            return message.reply("백준 ID를 입력해주세요. 예: `/message rlatjwls3333`");
+        }
+        const handle = parts[1];
+
+        userRegister(handle);
+        message.reply(`${handle}님의 문제 풀이 기록 저장 중...`);
+        message.reply(`${handle}님의 푼 문제 저장 완료`);
     }
 
     if (message.content == "말해봐") {
@@ -55,28 +67,34 @@ client.on('messageCreate', (message) => {
 client.login(token)
 
 
-function checkAllUser() {
-    if (updateUser) {
-        currentActiveUser = getJson();
-        updateUser = false
-    }
+// function checkAllUser() {
+//     if (updateUser) {
+//         currentActiveUser = getJson();
+//         updateUser = false
+//     }
 
-    for (let user in currentActiveUser) {
-        const recent = getRecentSolved(user);
+//     for (let user in currentActiveUser) {
+//         const recent = getRecentSolved(user);
 
-        recent.then((res) => {
-            if (recent === '') {
-                reject(new Error());
-            }
+//         recent.then((res) => {
+//             if (recent === '') {
+//                 reject(new Error());
+//             }
 
-            let msg = '최근 푼 문제 : \n';
-            for (const problem of res) {
-                msg += `• [${problem.rank}] ${problem.title} (${problem.url})\n`;
-            }
+//             let msg = '최근 푼 문제 : \n';
+//             for (const problem of res) {
+//                 msg += `• [${problem.rank}] ${problem.title} (${problem.url})\n`;
+//             }
 
-            channel.send(msg);
-        }).catch((err) => {
-            channel.send("제대로된 핸들을 입력하세요")
-        })
-    }
+//             channel.send(msg);
+//         }).catch((err) => {
+//             channel.send("제대로된 핸들을 입력하세요")
+//         })
+//     }
+// }
+
+function userRegister(handle) {
+    firstJoin(handle).then((res) => {
+        channel.send(`Welcome: ${handle}`)
+    })
 }
