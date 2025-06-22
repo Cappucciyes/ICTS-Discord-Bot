7f1334ba2794a23bbda2c1a1af2cf984ba38deaa
Cappucciyes <ltkproper@gmail.com>
2025-06-23 03:21:51 +0900

    intergrated baekjoon API

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // 1. 주요 클래스 가져오기
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { getJson, getRecentSolved } = require('./baekjoon.js');
 require('dotenv').config();
 const token = process.env.DISCORD_TOKEN;
 const serverID = process.env.SERVER_ID;
+
+let updateUser = true;
+let currentActiveUser = {}
 
 let inter;
 let channel
@@ -19,21 +23,21 @@ const client = new Client({
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
     channel = client.channels.cache.get(serverID)
-
-    inter = setInterval(() => {
-        channel.send('Hello Server!');
-    }, 1000);
 });
 
 // 4. 누군가 ping을 작성하면 pong으로 답장한다.
 client.on('messageCreate', (message) => {
-    if (message.content.startsWith('/등록')) {
+    if (message.content.startsWith('/최근푼')) {
         message.reply('bbbb');
 
     }
 
-    if (message.content.match("이제 그만")) {
+    if (message.content.startsWith("/새로푼")) {
         clearInterval(inter)
+    }
+
+    if (message.content == "/모두푼") {
+        checkAllUser();
     }
 
     if (message.content == "말해봐") {
@@ -41,7 +45,38 @@ client.on('messageCreate', (message) => {
             channel.send('안녕? 테스트 메시지야!');
         }, 1000);
     }
+
+    if (message.content == "그만") {
+        clearInterval(inter);
+    }
 })
+
 // 5. 시크릿키(토큰)을 통해 봇 로그인 실행
 client.login(token)
 
+
+function checkAllUser() {
+    if (updateUser) {
+        currentActiveUser = getJson();
+        updateUser = false
+    }
+
+    for (let user in currentActiveUser) {
+        const recent = getRecentSolved(user);
+
+        recent.then((res) => {
+            if (recent === '') {
+                reject(new Error());
+            }
+
+            let msg = '최근 푼 문제 : \n';
+            for (const problem of res) {
+                msg += `• [${problem.rank}] ${problem.title} (${problem.url})\n`;
+            }
+
+            channel.send(msg);
+        }).catch((err) => {
+            channel.send("제대로된 핸들을 입력하세요")
+        })
+    }
+}
