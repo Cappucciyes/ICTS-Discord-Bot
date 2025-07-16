5c81dd331c14fa29352730f52a49733ea0374dec
Cappucciyes <ltkproper@gmail.com>
2025-07-16 18:22:56 +0900

    made register, update command only available to the admin

--- commands/register.js
+++ commands/register.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, MessageFlags} = require("discord.js")
+const {SlashCommandBuilder, MessageFlags, PermissionsBitField } = require("discord.js")
 const {registerUser} =require("./../db.js")
 
 module.exports = {
@@ -17,15 +17,24 @@ module.exports = {
     async execute(interaction) {
         const handle = interaction.options.getString("user_id");
         const name = interaction.options.getString("user_name");
-        await interaction.reply({ content: `${handle}님의 정보 저장 중...`, flags: MessageFlags.Ephemeral })
-        registerUser(handle, name).then((res) => {
-            if (res) {
-                interaction.followUp({ content: `${handle}님의 푼 문제 저장 완료`, flags: MessageFlags.Ephemeral });
-            } else {
-                interaction.followUp({ content: `${handle}은 이미 등록하셨네요`, flags: MessageFlags.Ephemeral });
-            }
-        }).catch((err)=>{
-            interaction.followUp({content:`문제 발생! 방장에게 간단한 상황설명과 에러 메세지를 보내주세요!\n error: ${err.message}`,flags: MessageFlags.Ephemeral })
-        })
+
+        const interactionUser = await interaction.guild.members.fetch(interaction.user.id)
+
+        if (interactionUser.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            await interaction.reply({ content: `${handle}님의 정보 저장 중...`, flags: MessageFlags.Ephemeral })
+            registerUser(handle, name).then((res) => {
+                if (res) {
+                    interaction.followUp({ content: `${handle}님의 푼 문제 저장 완료`, flags: MessageFlags.Ephemeral });
+                } else {
+                    interaction.followUp({ content: `${handle}은 이미 등록하셨네요`, flags: MessageFlags.Ephemeral });
+                }
+            }).catch((err)=>{
+                interaction.followUp({content:`문제 발생! 방장에게 간단한 상황설명과 에러 메세지를 보내주세요!\n error: ${err.message}`,flags: MessageFlags.Ephemeral })
+            })
+        } else {
+            await interaction.reply({ content: "register 명령어의 권한이 없습니다!", flags: MessageFlags.Ephemeral })
+        }
+
+        
 	}
 };
--- commands/update.js
+++ commands/update.js
@@ -12,16 +12,23 @@ module.exports = {
 
     async execute(interaction) {
         const handle = interaction.options.getString("user_id");
-        await interaction.reply({ content: `${handle}님의 정보 갱신 중...`, flags: MessageFlags.Ephemeral })
-        let updatingTime = new Date();
-        updateUser(handle, updatingTime).then((res) => {
-            if (res) {
-                interaction.followUp(`${handle}님의 정보 갱신 완료!\n 이번 주 ${res["stat"]["weeklySolvedCount"]}문제를 푸셨습니다!\n 총 ${res["currentData"]["solvedCount"]} 문제를 푸셨습니다!`);
-            } else {
-                interaction.followUp({ content: `${handle}은 이미 등록하셨네요`, flags: MessageFlags.Ephemeral });
-            }
-        }).catch((err)=>{
-            interaction.followUp({content:`갱신 중 문제 발생! 방장에게 간단한 상황설명과 에러 메세지를 보내주세요!\n error: ${err.message}`,flags: MessageFlags.Ephemeral })
-        })
+
+        const interactionUser = await interaction.guild.members.fetch(interaction.user.id)
+
+        if (interactionUser.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            await interaction.reply({ content: `${handle}님의 정보 갱신 중...`, flags: MessageFlags.Ephemeral })
+            let updatingTime = new Date();
+            updateUser(handle, updatingTime).then((res) => {
+                if (res) {
+                    interaction.followUp(`${handle}님의 정보 갱신 완료!\n 이번 주 ${res["stat"]["weeklySolvedCount"]}문제를 푸셨습니다!\n 총 ${res["currentData"]["solvedCount"]} 문제를 푸셨습니다!`);
+                } else {
+                    interaction.followUp({ content: `${handle}은 이미 등록하셨네요`, flags: MessageFlags.Ephemeral });
+                }
+            }).catch((err)=>{
+                interaction.followUp({content:`갱신 중 문제 발생! 방장에게 간단한 상황설명과 에러 메세지를 보내주세요!\n error: ${err.message}`,flags: MessageFlags.Ephemeral })
+            })
+        } else {
+            await interaction.reply({ content: "update 명령어의 권한이 없습니다!", flags: MessageFlags.Ephemeral })
+        }
 	}
 };
